a7b4dee46f98053b4db84754bd70aac24aff97a3
darayavahush_ <internautuld.17@gmail.com>
2022-05-19 18:13:21 +0300

    Added main menu volume and vibrate options

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
+
+    public Image original;
+    public Sprite newSprite;
+    public Sprite oldSprite;
+    private bool isOriginal = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldSprite = original.sprite;
+    }
 
     void Awake()
     {
@@ -31,6 +44,22 @@ public class AudioManager : MonoBehaviour
         {
             // Stops playing music in GameScene scene
             Destroy(gameObject);
+        }
+    }
+
+    public void MuteToggle()
+    {
+        if (isOriginal == true)
+        {
+            original.sprite = newSprite;
+            isOriginal = false;
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            original.sprite = oldSprite;
+            isOriginal = true;
+            AudioListener.volume = 1;
         }
     }
 }
